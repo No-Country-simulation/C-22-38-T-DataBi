a0300478f6a2cb3d148c3e6d2dc82a533c4f3a61
Elsa Beltran <vivanco.elsa@gmail.com>
2024-11-30 10:57:57 -0300

    ajustes menu y rutas

--- phishing/urls.py
+++ phishing/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path 
-from .views import (home_view)
+from .views import (home_view,phishing_view)
 
 
 urlpatterns = [
-    path('',home_view,name='home')
+    path('',home_view,name='home'),
+    path('phishingurl/',phishing_view,name='phishingurl')
 ]
--- phishing2024/urls.py
+++ phishing2024/urls.py
@@ -21,5 +21,5 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('phishing.urls'))
+    path('phishing/',include('phishing.urls'))
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
