543fe88aad4296a5a7d0ab81671bc889bbc2edc0
Elsa Beltran <vivanco.elsa@gmail.com>
2024-12-10 06:52:05 -0300

    ajuste en templates presentacion

--- phishing/urls.py
+++ phishing/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path 
-from .views import (phishing_view)
+from .views import (phishing_view,home_view)
 
 
 urlpatterns = [
+    path('',home_view,name='home'),
     path('phishingurl/',phishing_view,name='phishingurl')
 ]
--- phishing/views.py
+++ phishing/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def home_view(request):
+    return render(request,'home.html')
+
 
 def phishing_view(request):
     return render(request,'phishingurl24.html')
