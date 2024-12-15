8574b58755b03554c94d6e0158b4ca9d46bbf7f2
Elsa Beltran <vivanco.elsa@gmail.com>
2024-12-15 04:48:10 -0300

    agregando imagenes a pagina web_07

--- phishing/static/js/index.js
+++ phishing/static/js/index.js
@@ -1,2 +1,12 @@
-$("#phishing").prop("type", "text");
-<><script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script><input type="hidden" id="phishing" /></>
+
+const menuButton = document.getElementById('menuButton');
+const menu = document.getElementById('menu');
+
+menuButton.addEventListener('click', () => {
+    // Alterna la visibilidad del menú
+    if (menu.style.display === 'none' || menu.style.display === '') {
+        menu.style.display = 'flex'; // Muestra el menú
+    } else {
+        menu.style.display = 'none'; // Oculta el menú
+    }
+});
